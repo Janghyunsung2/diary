1c0ae9c512e73e6daee117f6139db005b23182ab
Janghyunsung2 <tlswlfk@gmail.com>
2025-08-28 15:50:12 +0900

    jwt인증 리팩토링

--- src/main/kotlin/org/yojung/diary/common/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/org/yojung/diary/common/security/JwtAuthenticationFilter.kt
@@ -8,7 +8,6 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import org.yojung.diary.common.security.context.UserContext
 
 @Component
 class JwtAuthenticationFilter(
