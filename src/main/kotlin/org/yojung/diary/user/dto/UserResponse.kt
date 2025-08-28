5c72d2fbba57c6bc917af9a480b53711216fcd60
Janghyunsung2 <tlswlfk@gmail.com>
2025-08-28 14:22:54 +0900

    user response npe 해결

--- src/main/kotlin/org/yojung/diary/user/dto/UserResponse.kt
+++ src/main/kotlin/org/yojung/diary/user/dto/UserResponse.kt
@@ -5,10 +5,10 @@ import org.yojung.diary.user.domain.Gender
 // 회원 응답 DTO
 data class UserResponse(
     val id: Long?,
-    val oauthId: String,
-    val provider: String,
-    val nickname: String,
-    val profileImage: String,
-    val gender: Gender
+    val oauthId: String?,
+    val provider: String?,
+    val nickname: String?,
+    val profileImage: String?,
+    val gender: Gender?
 )
 
